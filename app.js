671a5f6075457acaaf1c86a0bb20f6111428d2cb
dc <danielmcollins1996@gmail.com>
2020-07-11 18:09:23 -0600

    fetch

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 class Jedi extends React.Component {
     render() {
       return (
@@ -7,14 +8,78 @@ class Jedi extends React.Component {
   }
   
   class Application extends React.Component {
-    render() {
-      return (
-        <main>
-            <h1>Vanilla JavaScript App</h1>
-            <p>Google analytics</p>
-        </main>
-      );
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          error: null,
+          isLoaded: false,
+          conferences: []
+        };
     }
+        componentDidMount() {
+            fetch("https://statsapi.web.nhl.com/api/v1/standings")
+              .then(res => res.json())
+              .then(
+                (result) => {
+                    console.log(result.records)
+                    this.setState({
+                    isLoaded: true,
+                    conferences: result.records
+                  });
+                },
+                (error) => {
+                  this.setState({
+                    isLoaded: true,
+                    error
+                  });
+                })
+            }
+            render() {
+                
+                const { error, isLoaded, conferences } = this.state;
+                if (error) {
+                  return <div>Error: {error.message}</div>;
+                } else if (!isLoaded) {
+                  return <div>Loading...</div>;
+                } else {
+                  return (
+                    <main>
+                        <h1>Vanilla JavaScript App</h1>
+                        <p>Google analytics</p>
+                        <div className="conferences">
+                        {conferences.map(item => (
+                          <div>
+                          <h3>{item.division.name}</h3>
+                            <ul>
+                            {item.teamRecords.map(team => {
+                                return (
+                                    <li key={team.team.id}>
+                                        {team.team.name}
+                                    </li>
+                                )
+                            })}
+                            </ul>
+                        </div>
+                        ))}
+                        </div>
+                        </main>
+                      )
+                }
+
+            // render() {
+    //   return (
+    //     <main>
+    //         <h1>Vanilla JavaScript App</h1>
+    //         <p>Google analytics</p>
+            
+    //         {conferences.map((team) => {
+    //             return console.log(team)
+    //         })}
+    //     </main>
+    //   );
+    // }
   }
+}
   
   ReactDOM.render(<Application />, document.getElementById('app'));
